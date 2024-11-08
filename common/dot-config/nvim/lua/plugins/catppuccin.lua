d0ab2fbc19752e08455576147ddcf34d9886f39c
wimblewamble <s3784709@student.rmit.edu.au>
2024-11-09 09:12:34 +1100

    Fix config

--- common/dot-config/nvim/lua/plugins/catppuccin.lua
+++ common/dot-config/nvim/lua/plugins/catppuccin.lua
@@ -5,7 +5,7 @@ return {
         priority = 1000,
         opts = {
             flavour = "mocha",
-            transparent_background = false,
+            transparent_background = true,
             dim_inactive = {
                 enabled = false,
             },
